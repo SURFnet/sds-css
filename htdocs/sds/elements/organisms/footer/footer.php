0a2ce95f531ea3264d4e62bfd8b478f0cf323173
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-05 16:30:46 +0200

    Moves footer. Refs #58098

---
+++ htdocs/sds/elements/organisms/footer/footer.php
@@ -0,0 +1,45 @@
+<footer>
+  <div class="footer--top">
+    <div class="grid-container">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Over SURF</a>
+          </li>
+          <li>
+            <a href="#">Werken bij</a>
+          </li>
+          <li>
+            <a href="#">Contact en route</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <div class="footer--bottom">
+    <div class="grid-container">
+      <div class="footer--bottom--inner">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Auteursrecht</a>
+            </li>
+            <li>
+              <a href="#">Voorwaarden</a>
+            </li>
+            <li>
+              <a href="#">Cookies</a>
+            </li>
+            <li>
+              <a href="#">Privacy</a>
+            </li>
+            <li>
+              <a href="#">Disclaimer</a>
+            </li>
+          </ul>
+        </nav>
+        <?php include("./sds/assets/images/logo-surf.svg"); ?>
+      </div>
+    </div>
+  </div>
+</footer>
