d996c9091006336a8c1b16f34dc299d825b5c9b2
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-05 09:36:52 +0200

    Adds index. Refs #58072

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="nl" dir="ltr">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="SURF Design System - Demo site" />
+  <meta name="generator" content="LimoenGroen" />
+  <title>SURF Design System | Demo en Development</title>
+</head>
+<body>
+
+<div class="page">
+
+  <article class="section">
+    <h2>Organisms</h2>
+    <h3>Footer</h3>
+    <xmp><?php include("assets/organisms/footer.html"); ?></xmp>
+    <?php include("assets/organisms/footer.html"); ?>
+  </article>
+
+</div>
+
+</body>
+</html>
