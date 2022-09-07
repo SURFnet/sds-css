6c06a66274f9b9b1e7b99d250f82de2c2abc0259
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-07 09:13:27 +0200

    Update index page. Refs #58233

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,18 +6,28 @@
   <meta name="description" content="SURF Design System - Demo site" />
   <meta name="generator" content="LimoenGroen" />
   <title>SURF Design System | Demo en Development</title>
-  <link rel="stylesheet" type="text/css" href="/htdocs/assets/index.css">
-  <link rel="stylesheet" type="text/css" href="/sds/assets/stylesheets/sds.css">
+  <link rel="stylesheet" type="text/css" href="./assets/index.css">
+  <link rel="stylesheet" type="text/css" href="./sds/assets/stylesheets/sds.css">
 </head>
 <body>
 
-<div class="page">
+<div class="page grid-container">
+  <h1>SURF Design System</h1>
+  <h2>Organisms</h2>
 
   <article class="section">
-    <h2>Organisms</h2>
     <h3>Footer</h3>
-    <xmp><?php include("../sds/organisms/footer.php"); ?></xmp>
-    <?php include("../sds/organisms/footer.php"); ?>
+    <div class="breakout-to-page">
+      <?php include("./sds/elements/organisms/footer/footer.php"); ?>
+    </div>
+    <details>
+      <summary>HTML</summary>
+      <xmp><?php include("./sds/elements/organisms/footer/footer.php"); ?></xmp>
+    </details>
+    <details>
+      <summary>SASS</summary>
+      <xmp><?php include("./sds/elements/organisms/footer/footer.scss"); ?></xmp>
+    </details>
   </article>
 
 </div>
