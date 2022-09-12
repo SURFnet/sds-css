722c517b212ea08f42bb5390df2566e3fea58eeb
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-12 14:55:29 +0200

    Adds button. Refs #58197

---
+++ htdocs/sds/elements/components/button/button.php
@@ -0,0 +1,16 @@
+<p>Variants</p>
+<div class="actions--outer">
+  <button class="btn btn--primary">Primary button</button>
+  <button class="btn btn--primary" disabled>Primary button disabled</button>
+  <button class="btn btn--secondary">Secondary button</button>
+  <button class="btn btn--secondary" disabled>Secondary button disabled</button>
+  <button class="btn btn--tertiary">Tertiary button</button>
+  <button class="btn btn--tertiary" disabled>Tertiary button disabled</button>
+</div>
+<p>Formats</p>
+<div class="actions--outer">
+  <button class="btn btn--primary">Default</button>
+  <button class="btn btn--primary btn--small">Small</button>
+  <button class="btn btn--primary btn--full">Full</button>
+</div>
+TODO: add icon.
