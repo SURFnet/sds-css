d192aff4ae34b0920cecab8208dec53c0d6c7d34
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-13 10:57:25 +0200

    Adds color. Refs #58189

---
+++ htdocs/sds/elements/foundation/color/color.php
@@ -0,0 +1,13 @@
+<p>Normal text, with no color-palette applied</p>
+
+<div class="color-palette--green">
+  <p style="color:var(--palette--main-color--400);">Normal text, with palette-color applied.</p>
+</div>
+
+<div class="color-palette--red">
+  <p style="color:var(--palette--main-color--400);">Normal text, with palette-color applied.</p>
+</div>
+
+<div class="color-palette--purple">
+  <p style="color:var(--palette--main-color--400);">Normal text, with palette-color applied.</p>
+</div>
