71658e6aa770607b855dbc9301100e042b4fcf00
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-19 13:58:04 +0200

    Adds alerts. Refs #58194

---
+++ htdocs/sds/elements/components/alert/alert.php
@@ -0,0 +1,74 @@
+<h5>Anatomy</h5>
+<div class="alert alert--status-default alert--has-action">
+  <div class="page-container">
+    <div class="alert--inner">
+      <div class="alert--visual">
+        <?php include("./sds/assets/images/functional-icons/info.svg"); ?>
+      </div>
+      <div class="alert--textual">
+        <p>It’s recommended to add at least one other administrator besides yourself.</p>
+        <button class="btn btn--primary btn--small">Add administrator</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<h5>Variants</h5>
+<div class="alert alert--status-default">
+  <div class="page-container">
+    <div class="alert--inner">
+      <div class="alert--visual">
+        <?php include("./sds/assets/images/functional-icons/info.svg"); ?>
+      </div>
+      <div class="alert--textual">
+        <p>Default</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="alert alert--status-success">
+  <div class="page-container">
+    <div class="alert--inner">
+      <div class="alert--visual">
+        <?php include("./sds/assets/images/functional-icons/success.svg"); ?>
+      </div>
+      <div class="alert--textual">
+        <p>Sucess</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="alert alert--status-warning">
+  <div class="page-container">
+    <div class="alert--inner">
+      <div class="alert--visual">
+        <?php include("./sds/assets/images/functional-icons/alert-triangle.svg"); ?>
+      </div>
+      <div class="alert--textual">
+        <p>Warning</p>
+      </div>
+    </div>
+  </div>
+</div><div class="alert alert--status-error">
+  <div class="page-container">
+    <div class="alert--inner">
+      <div class="alert--visual">
+        <?php include("./sds/assets/images/functional-icons/alert-triangle.svg"); ?>
+      </div>
+      <div class="alert--textual">
+        <p>Error</p>
+      </div>
+    </div>
+  </div>
+</div><div class="alert alert--status-info">
+  <div class="page-container">
+    <div class="alert--inner">
+      <div class="alert--visual">
+        <?php include("./sds/assets/images/functional-icons/info.svg"); ?>
+      </div>
+      <div class="alert--textual">
+        <p>Info</p>
+      </div>
+    </div>
+  </div>
+</div>
