6c903c191779de8f5e451a06330bc619c03dcecf
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-19 13:58:45 +0200

    Adds toaster. Refs #58207

---
+++ htdocs/sds/elements/components/toaster/toaster.php
@@ -0,0 +1,64 @@
+<h5>Anatomy</h5>
+<div class="demo-row">
+  <div class="toaster toaster--status-success toaster--has-action">
+    <div class="toaster--visual">
+      <?php include("./sds/assets/images/functional-icons/success.svg"); ?>
+    </div>
+    <div class="toaster--textual">
+      <p>Toaster with single line</p>
+      <button class="btn btn--secondary btn--small">Action</button>
+    </div>
+  </div>
+</div>
+
+<h5>Variants</h5>
+<div class="demo-row">
+  <div class="toaster toaster--status-default">
+    <div class="toaster--visual">
+      <?php include("./sds/assets/images/functional-icons/info.svg"); ?>
+    </div>
+    <div class="toaster--textual">
+      <p>Default</p>
+    </div>
+  </div>
+</div>
+<div class="demo-row">
+  <div class="toaster toaster--status-success">
+    <div class="toaster--visual">
+      <?php include("./sds/assets/images/functional-icons/success.svg"); ?>
+    </div>
+    <div class="toaster--textual">
+      <p>Default</p>
+    </div>
+  </div>
+</div>
+<div class="demo-row">
+  <div class="toaster toaster--status-warning">
+    <div class="toaster--visual">
+      <?php include("./sds/assets/images/functional-icons/alert-triangle.svg"); ?>
+    </div>
+    <div class="toaster--textual">
+      <p>Warning</p>
+    </div>
+  </div>
+</div>
+<div class="demo-row">
+  <div class="toaster toaster--status-error">
+    <div class="toaster--visual">
+      <?php include("./sds/assets/images/functional-icons/alert-triangle.svg"); ?>
+    </div>
+    <div class="toaster--textual">
+      <p>Error</p>
+    </div>
+  </div>
+</div>
+<div class="demo-row">
+  <div class="toaster toaster--status-info">
+    <div class="toaster--visual">
+      <?php include("./sds/assets/images/functional-icons/info.svg"); ?>
+    </div>
+    <div class="toaster--textual">
+      <p>Info</p>
+    </div>
+  </div>
+</div>
