f28662da21a6d9093cbb00628f9fad4c1854b3e4
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-19 14:44:25 +0200

    Adds badge. Refs #58195

---
+++ htdocs/sds/elements/components/badge/badge.php
@@ -0,0 +1,6 @@
+<span class="badge">
+  <span class="badge--visual">
+    <?php include("./sds/assets/images/functional-icons/allowance-no-talking.svg"); ?>
+  </span>
+  <span class="badge--textual text--body--small">badge text</span>
+</span>
