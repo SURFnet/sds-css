b46a2eac292c36f9f4f447ab3d900842e1cb1bb1
Martijn Broere <martijn.broere@limoengroen.nl>
2022-09-19 16:49:23 +0200

    Adds breadcrumb. Refs #58196

---
+++ htdocs/sds/elements/components/breadcrumb/breadcrumb.php
@@ -0,0 +1,19 @@
+<nav class="breadcrumb">
+  <ol class="breadcrumb--list">
+    <li>
+      <a href="#">Level 1</a>
+      <span class="breadcrumb--divider">
+        <?php include("./sds/assets/images/functional-icons/arrow-right-2.svg"); ?>
+      </span>
+    </li>
+    <li>
+      <a href="#">Level 2</a>
+      <span class="breadcrumb--divider">
+        <?php include("./sds/assets/images/functional-icons/arrow-right-2.svg"); ?>
+      </span>
+    </li>
+    <li class="breadcrumb--current">
+      <span>Level 3</span>
+    </li>
+  </ol>
+</nav>
