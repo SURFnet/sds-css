966f6cda2deb50d3498930809304d4c70c8e556f
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-03 14:38:55 +0200

    Adds chips. Refs #58199

---
+++ htdocs/sds/elements/components/chips/chips.php
@@ -0,0 +1,11 @@
+<div class="demo-row">
+  <span class="chips">
+    <span class="chips--visual">
+      <?php include("./sds/assets/images/functional-icons/alarm-bell.svg"); ?>
+    </span>
+    <span class="chips--textual text--body--small">Chip text</span>
+  </span>
+  <span class="chips chips--is-active">
+    <span class="chips--textual text--body--small">Chip text</span>
+  </span>
+</div>
