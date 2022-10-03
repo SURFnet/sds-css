937ad23cbd7549b72100103e03933b389c39eebf
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-03 11:05:57 +0200

    Adds checkbox. Refs #58198

---
+++ htdocs/sds/elements/components/checkbox/checkbox.php
@@ -0,0 +1,38 @@
+<div class="color-palette--blue">
+  <input id="unique-id" type="checkbox"></input>
+  <label for="unique-id">
+    <span class="checkbox--visual">
+      <span class="icon">
+        <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
+      </span>
+    </span>
+    <span class="checkbox--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
+  </label>
+  <input id="unique-id--2" type="checkbox" checked></input>
+  <label for="unique-id--2">
+    <span class="checkbox--visual">
+      <span class="icon">
+        <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
+      </span>
+    </span>
+    <span class="checkbox--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
+  </label>
+  <input id="unique-id--3" type="checkbox" disabled></input>
+  <label for="unique-id--3">
+    <span class="checkbox--visual">
+      <span class="icon">
+        <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
+      </span>
+    </span>
+    <span class="checkbox--text">Disabled</span>
+  </label>
+  <input id="unique-id--4" type="checkbox" disabled checked></input>
+  <label for="unique-id--4">
+    <span class="checkbox--visual">
+      <span class="icon">
+        <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
+      </span>
+    </span>
+    <span class="checkbox--text">Disabled</span>
+  </label>
+</div>
