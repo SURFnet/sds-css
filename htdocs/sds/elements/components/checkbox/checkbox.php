41a0eace8b63c717ff1f6b2d99987dde54c7217a
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-17 14:08:52 +0200

    Checkbox cleanup. Refs #58198

--- htdocs/sds/elements/components/checkbox/checkbox.php
+++ htdocs/sds/elements/components/checkbox/checkbox.php
@@ -1,26 +1,26 @@
-<input id="unique-id" type="checkbox"></input>
-<label for="unique-id">
+<input id="unique-checkbox-id" type="checkbox"></input>
+<label for="unique-checkbox-id">
   <span class="checkbox--visual">
     <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
   </span>
   <span class="checkbox--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
 </label>
-<input id="unique-id--2" type="checkbox" checked></input>
-<label for="unique-id--2">
+<input id="unique-checkbox-id--2" type="checkbox" checked></input>
+<label for="unique-checkbox-id--2">
   <span class="checkbox--visual">
     <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
   </span>
   <span class="checkbox--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
 </label>
-<input id="unique-id--3" type="checkbox" disabled></input>
-<label for="unique-id--3">
+<input id="unique-checkbox-id--3" type="checkbox" disabled></input>
+<label for="unique-checkbox-id--3">
   <span class="checkbox--visual">
     <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
   </span>
   <span class="checkbox--text">Disabled</span>
 </label>
-<input id="unique-id--4" type="checkbox" disabled checked></input>
-<label for="unique-id--4">
+<input id="unique-checkbox-id--4" type="checkbox" disabled checked></input>
+<label for="unique-checkbox-id--4">
   <span class="checkbox--visual">
     <?php include("./sds/assets/images/functional-icons/checkbox-check.svg"); ?>
   </span>
