91ec1ddc6ed8fde87a569bdb0151589bf64df983
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-17 12:36:22 +0200

    Adds modal. Refs #58201

---
+++ htdocs/sds/elements/components/modal/modal.php
@@ -0,0 +1,28 @@
+<div class="modal">
+  <div class="modal--container">
+    <div class="modal--title">
+      <p class="text--h4">Title</p>
+      <?php include("./sds/assets/images/functional-icons/close.svg"); ?>
+    </div>
+    <div class="alert alert--status-info">
+      <div class="alert--inner">
+        <div class="alert--visual">
+          <?php include("./sds/assets/images/functional-icons/info.svg"); ?>
+        </div>
+        <div class="alert--textual">
+          <p>Info</p>
+        </div>
+      </div>
+    </div>
+    <div class="modal--content">
+      <div class="text--rich">
+        <p>This is an example text that can be replaced when using a simple modal. You can also use a <a href="#">hyperlink</a> if it’s necessary. For more complex modals we advise you to use a Blank modal as base.</p>
+        <p>Second paragraph can be used if there is more text.</p>
+      </div>
+      <div class="modal--actions actions--outer">
+        <button class="btn btn--secondary">Secondary button</button>
+        <button class="btn btn--primary">Primary button</button>
+      </div>
+    </div>
+  </div>
+</div>
