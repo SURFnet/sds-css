3ceea60efa59d98b31986ee8a2cfe6bbbe3e25ff
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-17 14:07:06 +0200

    Adds radio. Refs #58202

---
+++ htdocs/sds/elements/components/radio/radio.php
@@ -0,0 +1,20 @@
+<input id="unique-radio-id" type="radio"></input>
+<label for="unique-radio-id">
+  <span class="radio--visual"></span>
+  <span class="radio--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
+</label>
+<input id="unique-radio-id--2" type="radio" checked></input>
+<label for="unique-radio-id--2">
+  <span class="radio--visual"></span>
+  <span class="radio--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
+</label>
+<input id="unique-radio-id--3" type="radio" disabled></input>
+<label for="unique-radio-id--3">
+  <span class="radio--visual"></span>
+  <span class="radio--text">Disabled</span>
+</label>
+<input id="unique-radio-id--4" type="radio" disabled checked></input>
+<label for="unique-radio-id--4">
+  <span class="radio--visual"></span>
+  <span class="radio--text">Disabled</span>
+</label>
