3988f94507415c0f384fd69c45220cb84d2134cd
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-18 09:11:36 +0200

    Adds tab-navigation. Refs #58204

---
+++ htdocs/sds/elements/components/tab-navigation/tab-navigation.php
@@ -0,0 +1,98 @@
+<div class="tab-navigation tab-navigation--default">
+  <nav class="tab-navigation--nav">
+    <ul>
+      <li>
+        <button class="active">
+          <span class="text--h5">Tab-item 1</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 2</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 3</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 4</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 5</span>
+        </button>
+      </li>
+    </ul>
+  </nav>
+</div>
+
+<div class="tab-navigation tab-navigation--bordered">
+  <nav class="tab-navigation--nav">
+    <ul>
+      <li>
+        <button class="active">
+          <span class="text--h5">Tab-item 1</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 2</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 3</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 4</span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span class="text--h5">Tab-item 5</span>
+        </button>
+      </li>
+    </ul>
+  </nav>
+  <div class="tab-navigation--content">
+    Tab content
+  </div>
+</div>
+
+<div class="tab-navigation tab-navigation--basic">
+  <nav class="tab-navigation--nav">
+    <ul>
+      <li>
+        <button class="btn btn--tertiary active">
+          <span class="text--h5">Tab-item 1</span>
+        </button>
+      </li>
+      <li>
+        <button class="btn btn--tertiary">
+          <span class="text--h5">Tab-item 2</span>
+        </button>
+      </li>
+      <li>
+        <button class="btn btn--tertiary">
+          <span class="text--h5">Tab-item 3</span>
+        </button>
+      </li>
+      <li>
+        <button class="btn btn--tertiary">
+          <span class="text--h5">Tab-item 4</span>
+        </button>
+      </li>
+      <li>
+        <button class="btn btn--tertiary">
+          <span class="text--h5">Tab-item 5</span>
+        </button>
+      </li>
+    </ul>
+  </nav>
+</div>
