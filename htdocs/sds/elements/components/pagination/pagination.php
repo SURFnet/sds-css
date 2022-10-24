eb5327bcde69462cd49e39b5b25440d33103f7e1
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-24 11:16:32 +0200

    Adds pagination. Refs #58209

---
+++ htdocs/sds/elements/components/pagination/pagination.php
@@ -0,0 +1,37 @@
+<div class="pagination">
+  <nav class="pagination--nav text--body--large">
+    <ul>
+      <li>
+        <a href="#">
+          <?php sizeable_svg("functional-icons/arrow-left-2", 7.29/18 . "em", 15/18 . "em"); ?>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="is-active">1</a>
+      </li>
+      <li>
+        <a href="#">2</a>
+      </li>
+      <li>
+        <a href="#">3</a>
+      </li>
+      <li>
+        <a href="#">4</a>
+      </li>
+      <li>
+        <a>5</a>
+      </li>
+      <li>
+        <a href="#">6</a>
+      </li>
+      <li>
+        <a href="#">...</a>
+      </li>
+      <li>
+        <a href="#">
+          <?php sizeable_svg("functional-icons/arrow-right-2", 7.29/18 . "em", 15/18 . "em"); ?>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
