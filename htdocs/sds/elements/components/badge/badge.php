47eaf9968249f83baaf7488f74f36827427c295a
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-24 12:26:49 +0200

    Adds tooltip. Refs #58208

--- htdocs/sds/elements/components/badge/badge.php
+++ htdocs/sds/elements/components/badge/badge.php
@@ -1,6 +1,6 @@
-<span class="badge">
+<div class="badge">
   <span class="badge--visual">
     <?php sizeable_svg("functional-icons/allowance-no-talking", 16.67/16 . "em", 16.67/16 . "em"); ?>
   </span>
   <span class="badge--textual text--body--small">badge text</span>
-</span>
+</div>
--- htdocs/sds/elements/components/chips/chips.php
+++ htdocs/sds/elements/components/chips/chips.php
@@ -1,11 +1,11 @@
 <div class="demo-row">
-  <span class="chips">
+  <div class="chips">
     <span class="chips--visual">
       <?php sizeable_svg("functional-icons/alarm-bell", 15/16 . "em", 18.75/16 . "em"); ?>
     </span>
     <span class="chips--textual text--body--small">Chip text</span>
-  </span>
-  <span class="chips chips--is-active">
+  </div>
+  <div class="chips chips--is-active">
     <span class="chips--textual text--body--small">Chip text</span>
-  </span>
+  </div>
 </div>
