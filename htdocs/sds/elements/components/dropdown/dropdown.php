6f9071f7b6a635febfa5b9711f31759d6fe43242
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-25 09:44:11 +0200

    Adds dropdown. Refs #58200

---
+++ htdocs/sds/elements/components/dropdown/dropdown.php
@@ -0,0 +1,54 @@
+<div class="select">
+  <div class="select--input-and-icon">
+    <select name="unique-select-name">
+      <option value="menu-item-1">Menu-item 1</option>
+      <option value="menu-item-2">Menu-item 2</option>
+      <option value="menu-item-3">Menu-item 3</option>
+      <option value="menu-item-4">Menu-item 4</option>
+    </select>
+    <span class="select--icon">
+      <?php sizeable_svg("functional-icons/arrow-down-2", 15/16 . "em", 7.29/16 . "em"); ?>
+    </span>
+  </div>
+</div>
+<div class="select">
+  <div class="select--input-and-icon">
+    <select name="unique-select-name--2" disabled>
+      <option value="menu-item-1">Menu-item 1</option>
+      <option value="menu-item-2">Menu-item 2</option>
+      <option value="menu-item-3">Menu-item 3</option>
+      <option value="menu-item-4">Menu-item 4</option>
+    </select>
+    <span class="select--icon">
+      <?php sizeable_svg("functional-icons/arrow-down-2", 15/16 . "em", 7.29/16 . "em"); ?>
+    </span>
+  </div>
+</div>
+<div class="select select--status-error">
+  <div class="select--input-and-icon">
+    <select name="unique-select-name--3">
+      <option value="menu-item-1">Menu-item 1</option>
+      <option value="menu-item-2">Menu-item 2</option>
+      <option value="menu-item-3">Menu-item 3</option>
+      <option value="menu-item-4">Menu-item 4</option>
+    </select>
+    <span class="select--icon">
+      <?php sizeable_svg("functional-icons/arrow-down-2", 15/16 . "em", 7.29/16 . "em"); ?>
+    </span>
+  </div>
+  <p class="select--message">Error message</p>
+</div>
+<div class="select">
+  <label for="unique-select-id--4">Label</label>
+  <div class="select--input-and-icon">
+    <select id="unique-select-id--4" name="unique-select-name--4">
+      <option value="menu-item-1">Menu-item 1</option>
+      <option value="menu-item-2">Menu-item 2</option>
+      <option value="menu-item-3">Menu-item 3</option>
+      <option value="menu-item-4">Menu-item 4</option>
+    </select>
+    <span class="select--icon">
+      <?php sizeable_svg("functional-icons/arrow-down-2", 15/16 . "em", 7.29/16 . "em"); ?>
+    </span>
+  </div>
+</div>
