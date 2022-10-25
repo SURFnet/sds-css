be6c384858153321b5e25082518cd5881c88f3ae
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-25 12:42:23 +0200

    Tab-navigation a11y improvements. Refs #58204

--- htdocs/sds/elements/components/tab-navigation/tab-navigation.php
+++ htdocs/sds/elements/components/tab-navigation/tab-navigation.php
@@ -2,66 +2,49 @@
   <nav class="tab-navigation--nav">
     <ul>
       <li>
-        <button class="active">
-          <span class="text--h5">Tab-item 1</span>
-        </button>
+        <button id="unique-button-item--1" class="active" role="tab" aria-controls="unique-tab-item--1" aria-selected="true">Tab-item 1</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 2</span>
-        </button>
+        <button id="unique-button-item--2" role="tab" aria-controls="unique-tab-item--2" aria-selected="false">Tab-item 2</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 3</span>
-        </button>
+        <button id="unique-button-item--3" role="tab" aria-controls="unique-tab-item--3" aria-selected="false">Tab-item 3</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 4</span>
-        </button>
+        <button id="unique-button-item--4" role="tab" aria-controls="unique-tab-item--4" aria-selected="false">Tab-item 4</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 5</span>
-        </button>
+        <button id="unique-button-item--5" role="tab" aria-controls="unique-tab-item--5" aria-selected="false">Tab-item 5</button>
       </li>
     </ul>
   </nav>
+  <div id="unique-tab-item--1" class="tab-navigation--content" role="tabpanel" aria-labelledby="unique-button-item--1">
+    Tab 1 content
+  </div>
 </div>
 
 <div class="tab-navigation tab-navigation--bordered">
   <nav class="tab-navigation--nav">
     <ul>
       <li>
-        <button class="active">
-          <span class="text--h5">Tab-item 1</span>
-        </button>
+        <button id="unique-button-item--6" class="active" role="tab" aria-controls="unique-tab-item--6" aria-selected="true">Tab-item 1</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 2</span>
-        </button>
+        <button id="unique-button-item--7" role="tab" aria-controls="unique-tab-item--7" aria-selected="false">Tab-item 2</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 3</span>
-        </button>
+        <button id="unique-button-item--8" role="tab" aria-controls="unique-tab-item--8" aria-selected="false">Tab-item 3</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 4</span>
-        </button>
+        <button id="unique-button-item--9" role="tab" aria-controls="unique-tab-item--9" aria-selected="false">Tab-item 4</button>
       </li>
       <li>
-        <button>
-          <span class="text--h5">Tab-item 5</span>
-        </button>
+        <button id="unique-button-item--10" role="tab" aria-controls="unique-tab-item--10" aria-selected="false">Tab-item 5</button>
       </li>
     </ul>
   </nav>
-  <div class="tab-navigation--content">
-    Tab content
+  <div id="unique-tab-item--6" class="tab-navigation--content" role="tabpanel" aria-labelledby="unique-button-item--6">
+    Tab 1 content
   </div>
 </div>
 
@@ -69,30 +52,23 @@
   <nav class="tab-navigation--nav">
     <ul>
       <li>
-        <button class="btn btn--tertiary active">
-          <span class="text--h5">Tab-item 1</span>
-        </button>
+        <button id="unique-button-item--11" class="active" role="tab" aria-controls="unique-tab-item--11" aria-selected="true">Tab-item 1</button>
       </li>
       <li>
-        <button class="btn btn--tertiary">
-          <span class="text--h5">Tab-item 2</span>
-        </button>
+        <button id="unique-button-item--12" role="tab" aria-controls="unique-tab-item--12" aria-selected="false">Tab-item 2</button>
       </li>
       <li>
-        <button class="btn btn--tertiary">
-          <span class="text--h5">Tab-item 3</span>
-        </button>
+        <button id="unique-button-item--13" role="tab" aria-controls="unique-tab-item--13" aria-selected="false">Tab-item 3</button>
       </li>
       <li>
-        <button class="btn btn--tertiary">
-          <span class="text--h5">Tab-item 4</span>
-        </button>
+        <button id="unique-button-item--14" role="tab" aria-controls="unique-tab-item--14" aria-selected="false">Tab-item 4</button>
       </li>
       <li>
-        <button class="btn btn--tertiary">
-          <span class="text--h5">Tab-item 5</span>
-        </button>
+        <button id="unique-button-item--15" role="tab" aria-controls="unique-tab-item--15" aria-selected="false">Tab-item 5</button>
       </li>
     </ul>
   </nav>
+  <div id="unique-tab-item--11" class="tab-navigation--content" role="tabpanel" aria-labelledby="unique-button-item--11">
+    Tab 1 content
+  </div>
 </div>
