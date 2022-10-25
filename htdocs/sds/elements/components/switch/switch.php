e4d515e909dca66df0471883d1d92e45195f9480
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-25 10:01:49 +0200

    Switch a11y improvements. Refs #58203

--- htdocs/sds/elements/components/switch/switch.php
+++ htdocs/sds/elements/components/switch/switch.php
@@ -1,3 +1,3 @@
-<span class="switch"></span>
+<span class="switch" role="switch" aria-checked="false"></span>
 
-<span class="switch switch--active"></span>
+<span class="switch switch--active" role="switch" aria-checked="true"></span>
