4080d5af6f4f87d10e536a0dd9b0bfcd696662d2
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-26 10:49:01 +0200

    Adds object-header (WIP). Refs #58211

---
+++ htdocs/sds/elements/organisms/object-header/markup/object-header.php
@@ -0,0 +1,13 @@
+<div class="object-header">
+  <div class="page-container">
+    <div class="object-header--inner">
+      <?php include("./sds/elements/components/breadcrumb/markup/breadcrumb.php"); ?>
+      <h1 class="object-header--title">Detailpage title example with more than one line to show max length</h1>
+      <div class="object-header--actions">
+        <button type="button" class="btn btn--primary">Primary button</button>
+        <button type="button" class="btn btn--secondary">Secondary button</button>
+      </div>
+      <?php include("./sds/elements/components/tab-navigation/markup/tab-navigation--default.php"); ?>
+    </div>
+  </div>
+</div>
