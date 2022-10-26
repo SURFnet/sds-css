4b49f50650e07ff90094e9642e8c77b418314360
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-26 09:41:35 +0200

    Switch split up demo markup. Refs #58203

---
+++ htdocs/sds/elements/components/switch/markup/switch--active.php
@@ -0,0 +1,1 @@
+<span class="switch switch--active" role="switch" aria-checked="true"></span>
--- htdocs/sds/elements/components/switch/markup/switch.php
+++ htdocs/sds/elements/components/switch/markup/switch.php
@@ -1,3 +1,1 @@
 <span class="switch" role="switch" aria-checked="false"></span>
-
-<span class="switch switch--active" role="switch" aria-checked="true"></span>
