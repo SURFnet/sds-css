a42a5200355c2b1f5514b61da720634e90457615
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-26 09:38:09 +0200

    Adds table. Refs #58205

---
+++ htdocs/sds/elements/components/table/markup/table.php
@@ -0,0 +1,134 @@
+<table>
+  <thead>
+    <tr>
+      <th>
+        <div class="table-tr--actions">
+          <button type="button" class="btn-reset">
+            <span class="text">Title</span>
+            <span class="visual">
+              <?php sizeable_svg("functional-icons/arrow-up-3", 5.87/16 . "em", 5.02/16 . "em"); ?>
+              <?php sizeable_svg("functional-icons/arrow-down-3", 5.87/16 . "em", 5.02/16 . "em"); ?>
+            </span>
+          </button>
+        </div>
+      </th>
+      <th>Title</th>
+      <th>Title</th>
+      <th>Title</th>
+      <th>Title</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Regular text</td>
+      <td>Regular text</td>
+      <td><a href="#">Hyperlink</a></td>
+      <td></td>
+      <td>
+        <div class="table-td--actions">
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/add", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Add</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/edit", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Edit</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/bin", 21/16 . "em", 21/16 . "em"); ?>
+            <span class="visually-hidden">Delete</span>
+          </button>
+        </div>
+      </td>
+    </tr>
+    <tr>
+      <td>Regular text</td>
+      <td>Regular text</td>
+      <td><a href="#">Hyperlink</a></td>
+      <td><?php include("./sds/elements/components/badge/markup/badge.php"); ?></td>
+      <td>
+        <div class="table-td--actions">
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/add", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Add</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/edit", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Edit</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/bin", 21/16 . "em", 21/16 . "em"); ?>
+            <span class="visually-hidden">Delete</span>
+          </button>
+        </div>
+      </td>
+    </tr>
+    <tr>
+      <td>Regular text</td>
+      <td>Regular text</td>
+      <td><a href="#">Hyperlink</a></td>
+      <td><?php include("./sds/elements/components/badge/markup/badge.php"); ?></td>
+      <td>
+        <div class="table-td--actions">
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/add", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Add</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/edit", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Edit</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/bin", 21/16 . "em", 21/16 . "em"); ?>
+            <span class="visually-hidden">Delete</span>
+          </button>
+        </div>
+      </td>
+    </tr>
+    <tr>
+      <td>Regular text</td>
+      <td>Regular text</td>
+      <td><a href="#">Hyperlink</a></td>
+      <td></td>
+      <td>
+        <div class="table-td--actions">
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/add", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Add</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/edit", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Edit</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/bin", 21/16 . "em", 21/16 . "em"); ?>
+            <span class="visually-hidden">Delete</span>
+          </button>
+        </div>
+      </td>
+    </tr>
+    <tr class="is-active">
+      <td>Regular text</td>
+      <td>Regular text</td>
+      <td><a href="#">Hyperlink</a></td>
+      <td></td>
+      <td>
+        <div class="table-td--actions">
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/add", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Add</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/edit", 22.5/16 . "em", 22.5/16 . "em"); ?>
+            <span class="visually-hidden">Edit</span>
+          </button>
+          <button type="button" class="btn-reset">
+            <?php sizeable_svg("functional-icons/bin", 21/16 . "em", 21/16 . "em"); ?>
+            <span class="visually-hidden">Delete</span>
+          </button>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<?php include("./sds/elements/components/pagination/markup/pagination.php"); ?>
