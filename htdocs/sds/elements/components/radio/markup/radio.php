7ef66a5655a80c46b7bb716c2d646f8278916782
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-26 08:03:39 +0200

    Moves elements php files to "markup" folder. Refs #58233

--- htdocs/sds/elements/components/radio/markup/radio.php
+++ htdocs/sds/elements/components/radio/markup/radio.php
@@ -1,19 +1,19 @@
-<input id="unique-radio-id" type="radio"></input>
+<input id="unique-radio-id" type="radio"/>
 <label for="unique-radio-id">
   <span class="radio--visual"></span>
   <span class="radio--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
 </label>
-<input id="unique-radio-id--2" type="radio" checked></input>
+<input id="unique-radio-id--2" type="radio" checked/>
 <label for="unique-radio-id--2">
   <span class="radio--visual"></span>
   <span class="radio--text">Multiple lines for longer data. Donec vitae sapien ut libero venenatis faucibus. Pellentesque auctor neque nec urna. Pellentesque posuere. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Curabitur ullamcorper ultricies nisi.</span>
 </label>
-<input id="unique-radio-id--3" type="radio" disabled></input>
+<input id="unique-radio-id--3" type="radio" disabled/>
 <label for="unique-radio-id--3">
   <span class="radio--visual"></span>
   <span class="radio--text">Disabled</span>
 </label>
-<input id="unique-radio-id--4" type="radio" disabled checked></input>
+<input id="unique-radio-id--4" type="radio" disabled checked/>
 <label for="unique-radio-id--4">
   <span class="radio--visual"></span>
   <span class="radio--text">Disabled</span>
