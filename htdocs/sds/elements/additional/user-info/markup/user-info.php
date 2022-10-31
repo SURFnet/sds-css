19179415cd8967d434162486d8091b7fab064319
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-31 09:40:40 +0100

    Isolates user-info. Refs #58213

---
+++ htdocs/sds/elements/additional/user-info/markup/user-info.php
@@ -0,0 +1,9 @@
+<div class="user-info">
+  <div class="user-info--textual">
+    <p class="name">Username</p>
+    <p class="role text--body--small">Role or Organization</p>
+  </div>
+  <button type="button" class="user-info--button">
+    <?php sizeable_svg("functional-icons/arrow-down-2", 11.25/16 . "em", 5.47/16 . "em"); ?>
+  </button>
+</div>
--- htdocs/sds/elements/organisms/application-header/markup/application-header--logged-in.php
+++ htdocs/sds/elements/organisms/application-header/markup/application-header--logged-in.php
@@ -32,15 +32,7 @@
         <button type="button">
           <?php sizeable_svg("functional-icons/alarm-bell", 18/16 . "em", 18/16 . "em"); ?>
         </button>
-        <div class="user-info">
-          <div class="user-info--textual">
-            <p class="name">Username</p>
-            <p class="role text--body--small">Role or Organization</p>
-          </div>
-          <button type="button" class="user-info--button">
-            <?php sizeable_svg("functional-icons/arrow-down-2", 11.25/16 . "em", 5.47/16 . "em"); ?>
-          </button>
-        </div>
+        <?php include("./sds/elements/additional/user-info/markup/user-info.php"); ?>
       </div>
     </div>
   </div>
