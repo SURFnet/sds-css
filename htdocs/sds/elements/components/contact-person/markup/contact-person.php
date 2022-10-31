a398e57e80fa7d35ad17d6f59813692f77d2f21f
Martijn Broere <martijn.broere@limoengroen.nl>
2022-10-31 11:29:12 +0100

    Adds contact person. Refs #52818

---
+++ htdocs/sds/elements/components/contact-person/markup/contact-person.php
@@ -0,0 +1,10 @@
+<div class="contact-person">
+  <div class="contact-person--visual">
+    <img src="" alt="" width="40" height="40"/>
+  </div>
+  <div class="contact-person--textual">
+    <p>Sineke Vergeer</p>
+    <p class="text--body--small">+31 1234 567 890</p>
+    <a href="" class="text--body--small">naam@surf.nl</a>
+  </div>
+</div>
