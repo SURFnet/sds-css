f31c9f460d2cc5ec52dcac92da2470d2088a7e65
Martijn Broere <martijn.broere@limoengroen.nl>
2022-11-01 13:30:09 +0100

    Adds accordion. Refs #58223

---
+++ htdocs/sds/elements/components/accordion/markup/accordion.php
@@ -0,0 +1,35 @@
+<div class="accordion text--body--large">
+  <div class="accordion--item">
+    <button class="accordion--trigger btn-reset">
+      <span class="icon">
+        <?php sizeable_svg("functional-icons/arrow-down-2", 12/18 . "em", 5.6/18 . "em"); ?>
+      </span>
+      <span class="text">Test 7-10-2022 FAQ TOP wordt niet gevonden via zoekopdracht</span>
+    </button>
+    <div class="accordion--content">
+      Here goes the text that holds the content for this section of the accordion
+    </div>
+  </div>
+  <div class="accordion--item">
+    <button class="accordion--trigger btn-reset">
+      <span class="icon">
+        <?php sizeable_svg("functional-icons/arrow-down-2", 12/18 . "em", 5.6/18 . "em"); ?>
+      </span>
+      <span class="text">Test 7-10-2022 FAQ TOP wordt niet gevonden via zoekopdracht</span>
+    </button>
+    <div class="accordion--content">
+      Here goes the text that holds the content for this section of the accordion
+    </div>
+  </div>
+  <div class="accordion--item is-open">
+    <button class="accordion--trigger btn-reset">
+      <span class="icon">
+        <?php sizeable_svg("functional-icons/arrow-down-2", 12/18 . "em", 5.6/18 . "em"); ?>
+      </span>
+      <span class="text">Test 7-10-2022 FAQ TOP wordt niet gevonden via zoekopdracht</span>
+    </button>
+    <div class="accordion--content">
+      Here goes the text that holds the content for this section of the accordion
+    </div>
+  </div>
+</div>
