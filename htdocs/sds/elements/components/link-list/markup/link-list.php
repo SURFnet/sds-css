7dc48ef2723cea909638649c53aabc3ebc264a4e
Martijn Broere <martijn.broere@limoengroen.nl>
2022-11-01 16:17:55 +0100

    Link-list cleanup. Refs #58227

--- htdocs/sds/elements/components/link-list/markup/link-list.php
+++ htdocs/sds/elements/components/link-list/markup/link-list.php
@@ -1,30 +1,42 @@
 
-  <ul class="link-list link-list--lading">
-    <li><a href="#">
+<ul class="link-list link-list--lading">
+  <li>
+    <a href="#">
       <?php sizeable_svg("functional-icons/arrow-right-2", 5.4/16 . "em", 11.14/16 . "em"); ?>
       <span class="text">Chocolate Cake</span>
-  </a></li>
-    <li><a href="#">
+    </a>
+  </li>
+  <li>
+    <a href="#">
       <?php sizeable_svg("functional-icons/arrow-right-2", 5.4/16 . "em", 11.14/16 . "em"); ?>
       <span class="text">Black Forest Cake</span>
-  </a></li>
-    <li><a href="#">
+    </a>
+  </li>
+  <li>
+    <a href="#">
       <?php sizeable_svg("functional-icons/arrow-right-2", 5.4/16 . "em", 11.14/16 . "em"); ?>
       <span class="text">Pineapple Cake</span>
-  </a></li>
-  </ul>
+    </a>
+  </li>
+</ul>
 
-  <ul class="link-list link-list--generic">
-    <li><a href="#">
+<ul class="link-list link-list--generic">
+  <li>
+    <a href="#">
       <?php sizeable_svg("functional-icons/arrow-right-2", 5.4/16 . "em", 11.14/16 . "em"); ?>
       <span class="text">Fasten your seatbelt</span>
-  </a></li>
-    <li><a href="#">
+    </a>
+  </li>
+  <li>
+    <a href="#">
       <?php sizeable_svg("functional-icons/arrow-right-2", 5.4/16 . "em", 11.14/16 . "em"); ?>
       <span class="text">Starts the car's engine</span>
-  </a></li>
-    <li><a href="#">
+    </a>
+  </li>
+  <li>
+    <a href="#">
       <?php sizeable_svg("functional-icons/arrow-right-2", 5.4/16 . "em", 11.14/16 . "em"); ?>
       <span class="text">Look around and go</span>
-  </a></li>
-  </ul>
+    </a>
+  </li>
+</ul>
