93f38a7ca30955f331dc0a42eb0e78149971c08e
Martijn Broere <martijn.broere@limoengroen.nl>
2022-12-14 15:16:16 +0100

    Link styling.

---
+++ htdocs/sds/elements/components/link/markup/link.php
@@ -0,0 +1,4 @@
+<div class="sds--text--rich">
+  <p>Curabitur turpis. Cras varius. Aenean ut eros et nisl sagittis vestibulum. Curabitur nisi. Class <a href="#">default link styling</a> ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
+</div>
+<p>Pulvinar leo id risus vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem <button class="sds--lnk">link styling applied to button</button> condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit.</p>
