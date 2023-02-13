3851998704aa3b89c244439358c89f82ef3364c4
Martijn Broere <martijn.broere@limoengroen.nl>
2023-02-13 12:45:42 +0100

    Adds block-switch. Refs #61028

---
+++ htdocs/sds/elements/components/block-switch/markup/block-switch.php
@@ -0,0 +1,30 @@
+<div class="demo-row">
+  <button class="sds--block-switch is-active">
+    <span class="sds--block-switch--title">
+      <span class="icon"><?php sizeable_svg("illustrative-icons/hr", 32/16 . "em", 32/16 . "em"); ?></span>
+      <span class="text sds--text--h3">Public team</span>
+    </span>
+    <span class="sds--text--body--small">List team in public teams index. Others can request membership.</span>
+  </button>
+
+  <a class="sds--block-switch" href="#">
+    <p class="sds--block-switch--title">
+      <span class="icon"><?php sizeable_svg("illustrative-icons/hr", 32/16 . "em", 32/16 . "em"); ?></span>
+      <span class="text sds--text--h3">Public team</span>
+    </p>
+    <p class="sds--text--body--small">List team in public teams index. Others can request membership.</p>
+  </a>
+</div>
+
+<h5>Vertical</h5>
+<div class="demo-row">
+  <button class="sds--block-switch sds--block-switch--vertical">
+    <span class="sds--block-switch--visual">
+      <span class="icon"><?php sizeable_svg("illustrative-icons/hr", 32/16 . "em", 32/16 . "em"); ?></span>
+    </span>
+    <span class="sds--block-switch--textual">
+      <span class="sds--block-switch--title sds--text--h3">Public team</span>
+      <span class="sds--text--body--small">List team in public teams index. Others can request membership.</span>
+    </span>
+  </button>
+</div>
