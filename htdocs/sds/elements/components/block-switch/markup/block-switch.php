8fb584e1d08e0251597bff6320c47a1f31faa04d
MartijnBroere <martijn.broere@limoengroen.nl>
2023-07-10 07:51:27 +0200

    Block-switch give visual correct color when active.

--- htdocs/sds/elements/components/block-switch/markup/block-switch.php
+++ htdocs/sds/elements/components/block-switch/markup/block-switch.php
@@ -18,6 +18,15 @@
 
 <h5>Vertical</h5>
 <div class="demo-row">
+  <button class="sds--block-switch sds--block-switch--vertical is-active">
+    <span class="sds--block-switch--visual">
+      <span class="icon"><?php sizeable_svg("illustrative-icons/hr", 32/16 . "em", 32/16 . "em"); ?></span>
+    </span>
+    <span class="sds--block-switch--textual">
+      <span class="sds--block-switch--title sds--text--h3">Public team</span>
+      <span class="sds--text--body--small">List team in public teams index. Others can request membership.</span>
+    </span>
+  </button>
   <button class="sds--block-switch sds--block-switch--vertical">
     <span class="sds--block-switch--visual">
       <span class="icon"><?php sizeable_svg("illustrative-icons/hr", 32/16 . "em", 32/16 . "em"); ?></span>
